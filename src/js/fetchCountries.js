091780ac0d7bfd62b0f3d47bb718043bc592934e
Wlad <wladkrychin@gmail.com>
2021-06-25 09:12:00 +0200

    2

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,6 +4,8 @@ import countrySearch from './country-url';
 import articlesOneCountry from '../template/oneCountry.hbs';
 import countryList from '../template/manyCountry.hbs';
 import debounce from 'lodash.debounce';
+import { alert, defaultModules } from '../../node_modules/@pnotify/core/dist/PNotify.js';
+
  const searchForm = document.querySelector('.input-js');
  const articles  = document.querySelector('.js-articles');
 
@@ -19,10 +21,10 @@ function countrySearchInputHandler(e) {
   countrySearch.fetchArticles(searchQuery).then(data => {
     
       if (data.length > 10) {
-          console.log("Too many matches found. Please enter a more specific query!")
+          alert('Too many matches found. Please enter a more specific query!')
       }
        else if (data.status === 404) {
-    console.log("No country has been found. Please enter a more specific query!");
+    alert('No country has been found. Please enter a more specific query!');
       } 
       else if (data.length === 1) {
           buildListMarkup(data, articlesOneCountry);
@@ -32,8 +34,8 @@ function countrySearchInputHandler(e) {
       }
   })
   .catch(
-  console.log('ДАВАЙ ДОСВИДАНЬЯ'))
-  }
+alert('No country has been found. Please enter a more specific query!')
+  )}
 function buildListMarkup(countries, template) {
   const markup = countries.map(count => template(count)).join();
   articles.insertAdjacentHTML('afterbegin', markup)
